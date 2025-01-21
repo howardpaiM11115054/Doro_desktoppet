58c46c52a7590988072981df37cbbe7d7cd76ce4
howardpaiM11115054 <er0508i8500@gmail.com>
2025-01-21 23:06:01 +0800

    add kill and exit

--- pyqt5doro.py
+++ pyqt5doro.py
@@ -15,16 +15,20 @@ class Deskpet(QWidget):
         # Pet counters
         self.sleep_counter = 0
         self.dark_counter = 0
-        self.animation_type = 'walk'  # 預設動畫類型
+        self.death_counter=0
+        self.animation_type = 'walk'  # animation type
 
-        # 設置窗口透明和無邊框
+        # window for invisible
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.SubWindow)
         self.setAttribute(Qt.WA_TranslucentBackground, True)
         self.resize(900, 900)
 
-        # 初始化圖片
+
+
+        # init load
         self.image_label = QLabel(self)
-        self.image_label.setScaledContents(True)  # 自動縮放圖片以適應窗口大小
+        self.image_label.setScaledContents(True) 
+         # 自動縮放圖片以適應窗口大小
 
         # 加載圖片
         self.frames = self.load_frames("img")  # 替換為你的動畫圖片文件夾路徑
@@ -41,10 +45,11 @@ class Deskpet(QWidget):
             'walk': [],
             'dark': [],
             'death': [],
-            'sleep': []
+            'sleep': [],
+            'death':[]
         }
 
-        animation_types = ['walk', 'dark', 'death', 'sleep']
+        animation_types = ['walk', 'dark', 'death', 'sleep','death']
         for animation in animation_types:
             animation_folder = os.path.join(folder, animation)
             num_of_frames = len(os.listdir(animation_folder))-1
@@ -62,28 +67,34 @@ class Deskpet(QWidget):
 
     def animation_types(self):
         """更新當前的動畫類型"""
+        if self.death_counter > 0:
+            self.death_counter -= 1
+            self.animation_type = 'death'
+            return
         if self.sleep_counter > 0:
             self.sleep_counter -= 1
             self.animation_type = 'sleep'
-        elif self.dark_counter > 0:
+            return
+        if self.dark_counter > 0:
             self.dark_counter -= 1
             self.animation_type = 'dark'
+            return
+
+        # 隨機選擇動畫類型
+        if random.randint(1, 200) == 2:
+            self.animation_type = 'sleep'
+            self.sleep_counter = 40
+        elif random.randint(1, 100) == 1:
+            self.animation_type = 'dark'
+            self.dark_counter = 60
         else:
-            if random.randint(1, 200) == 2:
-                self.animation_type = 'sleep'
-                self.sleep_counter = 40
-            elif random.randint(1, 100) == 1:
-                self.animation_type = 'dark'
-                self.dark_counter = 60
-            else:
-                self.animation_type = 'walk'
-
+            self.animation_type = 'walk'
     def update_frame(self):
+        
         """切換到下一幀"""
-        self.animation_types()  # 更新動畫類型
-
         # 獲取當前動畫類型的幀列表
         current_frames = self.frames.get(self.animation_type, [])
+        self.animation_types()  # 更新動畫類型
         if not current_frames:  # 如果列表為空
             print(f"動畫類型 {self.animation_type} 沒有可用幀")
             return
@@ -115,6 +126,13 @@ class Deskpet(QWidget):
         """釋放滑鼠"""
         if event.button() == Qt.LeftButton:
             self.old_pos = None  # 清空舊位置
+    # def mousePressEvent(self, event): 這樣會沒辦法拉
+    #     """左鍵按下事件"""
+    #     if event.button() == Qt.LeftButton:  # 檢查是否是左鍵
+    #         print("e04")  # Debug 信息
+    #         self.animation_type = 'death'  # 切換動畫類型為 'death'
+    #         self.current_frame = 0  # 重置動畫幀索引，從頭開始播放動畫
+        
 
 
     def contextMenuEvent(self, event):
@@ -123,12 +141,17 @@ class Deskpet(QWidget):
         menu = QMenu(self)
 
         # 添加操作（QAction）
-        action_exit = menu.addAction("退出")  # 添加一個 "退出" 選項
-
+        action_exit = menu.addAction("EXIT")  # 添加一個 "退出" 選項
+        action_kill = menu.addAction("Kill")
         # 在鼠標位置顯示菜單
         action = menu.exec_(self.mapToGlobal(event.pos()))
 
         # 判斷選擇的選項
+        if action ==action_kill:
+            self.animation_type = 'death'  # 切換動畫類型為 'death'
+            self.current_frame = 0  # 重置動畫幀索引，從頭開始播放動畫
+            self.death_counter=50
+            # self.update_frame()
         if action == action_exit:
             self.close() 
             sys.exit(app.exec_()) # 如果選擇了 "退出"，則關閉窗口
